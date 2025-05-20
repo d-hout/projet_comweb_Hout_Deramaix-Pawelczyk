3e1d78c0d71ab60a2c185cce7a0033801d6af6e5
d-hout <dhout@ensc.fr>
2025-05-20 13:51:59 +0200

    Update eleves.php

--- API/eleves.php
+++ API/eleves.php
@@ -5,6 +5,7 @@ function envoiJSON($donnees)
     echo $json;
 }
 
+// Fonction pour récupérer les notes d’un élève à partir de son identifiant et mot de passe
 function recupererNotes($nom_utilisateur, $mdp)
 {
     header('Access-Control-Allow-Origin: *'); // Autorise toutes les origines
@@ -12,23 +13,31 @@ function recupererNotes($nom_utilisateur, $mdp)
     $dbname = 'bpawelczyk';
     $username = 'bpawelczyk';
     $password = 'avion';
+    
+     // Connexion à la base de données avec gestion d’erreur
     try {
         $bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8', $username, $password);
     } catch(Exception $e) {
         // Si erreur, tout arrêter
         die('Erreur : '. $e->getMessage());
     }
+    
+    // Requête SQL pour récupérer les notes de l’élève
     $requete = "SELECT nom_matiere, nom_controle, note, coefficient, commentaire
     FROM notes
     JOIN eleves ON notes.eleve_id=eleves.id
     WHERE nom_utilisateur_eleve = :nom_utilisateur AND mdp_eleve = :mdp
     ORDER BY nom_matiere";
+    
+    // Préparation et exécution de la requête
     $resultat = $bdd->prepare($requete);
     $resultat->execute([':nom_utilisateur' => $nom_utilisateur, ':mdp' => $mdp]);
+    // Récupération des résultats sous forme de tableau
     $tab = $resultat->fetchAll();
     envoiJSON($tab);
 }
 
+// Vérifie si les paramètres nom_utilisateur et mdp sont fournis dans l’URL
 if (empty($_GET['nom_utilisateur']) || empty($_GET['mdp']))
 {
     header("Content-Type: text/html; charset=UTF-8");
@@ -36,6 +45,7 @@ if (empty($_GET['nom_utilisateur']) || empty($_GET['mdp']))
 }
 else
 {
+    // Si les deux paramètres sont fournis, on envoie une réponse JSON avec les notes
     header('Content-Type: application/json');
     $nom_utilisateur = $_GET['nom_utilisateur'];
     $mdp = $_GET['mdp'];
